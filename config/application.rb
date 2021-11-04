d87e27506456ae276aa8a5667e27081261405f72
Santiago Bartesaghi <santib@hey.com>
2021-11-04 14:39:17 -0300

    Create app_base component

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module RailsModularMonolithWithDdd
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.paths.add 'components', glob: '*/app/{*,*/concerns}', eager_load: true
   end
 end
