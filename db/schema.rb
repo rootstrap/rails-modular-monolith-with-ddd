9451275a5e45ddab4988867b9cb8082643bac6cc
Santiago Bartesaghi <santib@hey.com>
2022-01-25 21:16:17 -0300

    Add annotate gem

--- db/schema.rb
+++ db/schema.rb
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_11_12_193241) do
     t.index ["email"], name: "index_user_access_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_user_access_users_on_reset_password_token", unique: true
   end
+
 end
