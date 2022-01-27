8adc59713f43ec877d428e72eba6a67dfe3650c3
Santiago Bartesaghi <santib@hey.com>
2022-01-27 17:06:13 -0300

    Override User registrations controller

---
+++ components/user_access/app/controllers/user_access/users/registrations_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module UserAccess
+  class Users::RegistrationsController < Devise::RegistrationsController
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, class_name: 'UserAccess::User'
+  devise_for :users, class_name: 'UserAccess::User', controllers: {
+    registrations: 'user_access/users/registrations'
+  }
 end
