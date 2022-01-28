2c0360c5e7fedf55159410e8fa0fe336a22eff04
Santiago Bartesaghi <santib@hey.com>
2022-01-28 17:06:32 -0300

    Annotate models

--- components/user_access/app/models/user_access/user_registration.rb
+++ components/user_access/app/models/user_access/user_registration.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: user_access_user_registrations
+#
+#  id                 :bigint           not null, primary key
+#  confirmed_at       :datetime
+#  email              :string           not null
+#  encrypted_password :string           not null
+#  first_name         :string           not null
+#  last_name          :string           not null
+#  login              :string           not null
+#  name               :string           not null
+#  registered_at      :datetime         not null
+#  status_code        :integer          not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 module UserAccess
   class UserRegistration < ApplicationRecord
     attr_reader :password, :current_password
