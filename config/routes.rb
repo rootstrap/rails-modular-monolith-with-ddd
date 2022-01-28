c8fd6bbfdc10567843ea0e7b2905aef42ea696fe
Santiago Bartesaghi <santib@hey.com>
2022-01-28 17:52:51 -0300

    Fix root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  root to: 'rails/welcome#index'
 
   devise_for :users, class_name: 'UserAccess::User', controllers: {
     registrations: 'user_access/users/registrations'
