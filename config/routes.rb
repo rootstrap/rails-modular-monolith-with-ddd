d5837a2657a5408e4cc7c7ad2a6d41cb3efe5b6f
Santiago Bartesaghi <santib@hey.com>
2022-02-17 21:56:48 -0300

    Improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'rails/welcome#index'
-
   devise_for :users, only: :sessions, class_name: 'UserAccess::User', controllers: {
     sessions: 'user_access/users/sessions'
   }
@@ -8,4 +6,7 @@ Rails.application.routes.draw do
     registrations: 'user_access/users/registrations',
     confirmations: 'user_access/users/confirmations'
   }
+  devise_scope :user do
+    root to: 'user_access/users/sessions#new'
+  end
 end
