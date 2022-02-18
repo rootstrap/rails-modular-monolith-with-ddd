f2b0d42b608a22c20af40a05191300cfa1d55cc8
Santiago Bartesaghi <santib@hey.com>
2022-02-18 10:11:34 -0300

    Remove devise code

--- components/user_access/app/models/user_access/user_registration.rb
+++ components/user_access/app/models/user_access/user_registration.rb
@@ -22,15 +22,11 @@
 #
 module UserAccess
   class UserRegistration < ApplicationRecord
-    attr_reader :password, :current_password
-    attr_accessor :password_confirmation
-
-    devise :registerable, :validatable, :confirmable
+    devise :database_authenticatable, :registerable, :validatable, :confirmable
 
     validates_uniqueness_of :email, :login
     validates_presence_of :email, :encrypted_password, :first_name, :last_name, :name,
                           :login, :registered_at, :status_code
-    validates_confirmation_of :password
 
     enum status_code: {
       waiting_for_confirmation: 0,
@@ -41,11 +37,6 @@ module UserAccess
     def initialize(*args, &block)
       super
       @skip_confirmation_notification = true
-    end
-
-    def password=(new_password)
-      @password = new_password
-      self.encrypted_password = password_digest(@password) if @password.present?
     end
 
     private
