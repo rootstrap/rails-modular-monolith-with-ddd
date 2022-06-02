ac6281b201c82c048dba42417eabb582b221dabe
Santiago Bartesaghi <santib@hey.com>
2022-06-02 17:01:44 -0300

    Split routes (#23)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, only: :sessions, class_name: 'UserAccess::User', controllers: {
-    sessions: 'user_access/users/sessions'
-  }
-  devise_for :user_registrations, only: [:registrations, :confirmations], path: :users, class_name: 'UserAccess::UserRegistration', controllers: {
-    registrations: 'user_access/users/registrations',
-    confirmations: 'user_access/users/confirmations'
-  }
+  draw(:user_access)
 
   authenticated do
     root to: 'rails/welcome#index'
