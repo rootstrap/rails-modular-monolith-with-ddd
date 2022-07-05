06e79d957690f6ed143430b77b93d2231a6559c5
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2022-07-05 20:20:35 -0300

    Remove now useless subscribers

--- components/meetings/config/initializers/pubsub/new_user_register.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.reloader.to_prepare do
-  ActiveSupport::Notifications.subscribe('new_user_registered_domain_event.user_access') do |event|
-    Meetings::CreateMemberService.new(event.payload).call
-  end
-end
