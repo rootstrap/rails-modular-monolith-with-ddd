0ef5e1b066159c2e12cf6133bf5ff6dcd234687d
Santiago Bartesaghi <santib@hey.com>
2022-09-01 16:32:06 -0300

    Delete everything except README

--- components/user_access/app/models/user_access/application_record.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module UserAccess
-  class ApplicationRecord < ::ApplicationRecord
-    self.abstract_class = true
-    self.table_name_prefix = 'user_access_'
-  end
-end
