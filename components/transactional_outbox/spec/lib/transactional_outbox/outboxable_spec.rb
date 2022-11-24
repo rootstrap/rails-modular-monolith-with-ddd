d1d1bdbe9546418fdef651295c7922341b4559da
Santiago Bartesaghi <santib@hey.com>
2022-11-24 17:22:18 -0300

    WIP - broken

--- components/transactional_outbox/spec/lib/transactional_outbox/outboxable_spec.rb
+++ components/transactional_outbox/spec/lib/transactional_outbox/outboxable_spec.rb
@@ -11,19 +11,22 @@ RSpec.describe TransactionalOutbox::Outboxable do
 
   before do
     ActiveRecord::Base.connection.create_table :fake_models do |t|
-      t.uuid :identifier
-      t.string :foo
+      t.uuid :identifier, null: false
     end
   end
 
   describe '#save' do
-    subject { FakeModel.new(identifier: SecureRandom.uuid, foo: foo).save }
+    subject { FakeModel.new(identifier: identifier).save }
 
     context 'when record is created' do
-      context 'when outbox record is created' do
-        let(:foo) { :bar }
+      let(:identifier) { SecureRandom.uuid }
 
+      context 'when outbox record is created' do
         it { is_expected.to be true }
+
+        it 'creates the record' do
+          expect { subject }.to change(FakeModel, :count).by(1)
+        end
 
         it 'creates the outbox record' do
           expect { subject }.to change(TransactionalOutbox::Outbox, :count).by(1)
@@ -32,15 +35,17 @@ RSpec.describe TransactionalOutbox::Outboxable do
 
       context 'when there is an error when creating the outbox record' do
         before do
-          # outbox save returns false/is invalid.
+          expect(TransactionalOutbox::Outbox).to receive(:create!).and_raise
         end
 
-        it 'does not create the record' do
+        it { is_expected.to be false }
 
+        it 'does not create the record' do
+          expect { subject }.to raise_error.and not_to change(FakeModel, :count)
         end
 
         it 'does not create the outbox record' do
-          expect { subject }.not_to change(TransactionalOutbox::Outbox, :count).by(1)
+          expect { subject }.to raise_error.and not_to change(TransactionalOutbox::Outbox, :count)
         end
       end
     end
