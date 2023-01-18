1ba1dc58e1ea57ab9931373efd51e42af1b63380
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-01-18 11:17:34 -0300

    Fix migration and move change to new one

--- db/migrate/20220125232600_add_columns_to_user_access_user.rb
+++ db/migrate/20220125232600_add_columns_to_user_access_user.rb
@@ -1,6 +1,7 @@
 class AddColumnsToUserAccessUser < ActiveRecord::Migration[6.1]
   def change
     add_column :user_access_users, :login, :string, null: false
+    add_column :user_access_users, :is_active, :boolean, null: false
     add_column :user_access_users, :first_name, :string, null: false
     add_column :user_access_users, :last_name, :string, null: false
     add_column :user_access_users, :name, :string, null: false
