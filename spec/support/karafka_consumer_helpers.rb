c79c8f5f7e60c3af8b56b7dd252fdec5d23bf564
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-02-14 11:30:42 -0300

    Refactor consumer definition helper

--- spec/support/karafka_consumer_helpers.rb
+++ spec/support/karafka_consumer_helpers.rb
@@ -1,15 +1,19 @@
 # Follow karafka.rb config to match the consumers defined here
 RSpec.shared_context 'Karafka consumer helpers' do
   let(:consumers) do
-    Karafka::App.routes.map do |cg|
-      next if cg.name == 'app'
-      cg.topics.map do |t|
-        Support::KarafkaConsumerMock.build(
-          t.consumer.new,
-          t.name,
-          _karafka_consumer_client
-        )
+    Karafka::App.routes
+      .reject { |cg| cg.name == 'app' } # We reject the default Karafka consumer
+      .flat_map(&:topics)
+      .flat_map do |topics|
+        topics.map { |topic| build_consumer_mock(topic) }
       end
-    end.flatten.compact
+  end
+
+  def build_consumer_mock(topic)
+    Support::KarafkaConsumerMock.build(
+      topic.consumer.new,
+      topic.name,
+      _karafka_consumer_client
+    )
   end
 end
