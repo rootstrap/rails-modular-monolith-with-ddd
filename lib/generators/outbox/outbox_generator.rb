893b6b064a9fdea2f769f5f5cc5da9a5a74f5d81
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2023-03-19 19:34:52 -0300

    Fix generator

--- lib/generators/outbox/outbox_generator.rb
+++ lib/generators/outbox/outbox_generator.rb
@@ -3,14 +3,13 @@ module Outbox
   class OutboxGenerator < ActiveRecord::Generators::Base
     source_root File.expand_path("../templates", __FILE__)
 
-    argument :domains, type: :array, default: [], banner: "domain domain"
-    class_option :root_components_path, type: :string, default: Rails.root
+    class_option :root_components_path, type: :string, default: Rails.root.to_s
 
     def create_migration_files
       migration_path = "#{options['root_components_path']}/db/migrate"
       migration_template(
         "migration.rb",
-        "#{migration_path}/outbox_create_#{table_name}.rb",
+        "#{migration_path}/outbox_create_#{table_name.singularize}.rb",
         migration_version: migration_version
       )
     end
