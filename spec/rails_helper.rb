dd29116ac579f15f0dd9575813e2996712e490e6
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2023-04-06 16:07:11 -0300

    Extract test setup to the transactional_outbox component

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -92,28 +92,4 @@ RSpec.configure do |config|
       end
     end
   end
-
-  config.define_derived_metadata(file_path: %r{components/transactional_outbox/spec/}) do |metadata|
-    metadata[:transactional_outbox] = true
-  end
-
-  config.before :each, transactional_outbox: true do
-    ActiveRecord::Base.connection.create_table :custom_outbox_test_models do |t|
-      t.uuid :identifier, null: false
-    end
-
-    ActiveRecord::Base.connection.create_table :default_outbox_test_models do |t|
-      t.uuid :identifier, null: false
-    end
-
-    ActiveRecord::Base.connection.create_table :custom_outbox_outboxes do |t|
-      t.uuid :identifier, null: false, index: { unique: true }
-      t.string :event, null: false
-      t.jsonb :payload
-      t.string :aggregate, null: false
-      t.uuid :aggregate_identifier, null: false
-
-      t.timestamps
-    end
-  end
 end
