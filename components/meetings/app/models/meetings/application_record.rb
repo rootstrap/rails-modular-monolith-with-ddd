936e9699d0e5247fb6434fa54830124b1c66e7cb
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2023-04-07 18:26:44 -0300

    Setup one DB per component

--- components/meetings/app/models/meetings/application_record.rb
+++ components/meetings/app/models/meetings/application_record.rb
@@ -4,5 +4,7 @@ module Meetings
   class ApplicationRecord < ::ApplicationRecord
     self.abstract_class = true
     self.table_name_prefix = 'meetings_'
+
+    connects_to database: { writing: :meetings, reading: :meetings }
   end
 end
