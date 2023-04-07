0748beb2d0db99a1a922de59037d2f356373bbae
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2023-04-07 18:44:56 -0300

    Separate migrations per DB

--- db/schema.rb
+++ db/schema.rb
@@ -14,68 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_185646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "meetings_consumed_messages", force: :cascade do |t|
-    t.uuid "event_id"
-    t.string "aggregate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "status", default: 0, null: false
-    t.index ["event_id", "aggregate"], name: "index_meetings_consumed_messages_on_event_id_and_aggregate", unique: true
-    t.index ["status"], name: "index_meetings_consumed_messages_on_status"
-  end
-
-  create_table "meetings_meeting_group_proposals", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
-    t.string "location_city", null: false
-    t.string "location_country_code", null: false
-    t.datetime "proposal_date", precision: nil, null: false
-    t.integer "status_code", null: false
-    t.bigint "proposal_user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.uuid "identifier", null: false
-    t.index ["proposal_user_id"], name: "index_meetings_meeting_group_proposals_on_proposal_user_id"
-  end
-
-  create_table "meetings_meeting_groups", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description"
-    t.string "location_city", null: false
-    t.string "location_country_code", null: false
-    t.bigint "creator_id", null: false
-    t.bigint "meeting_group_proposal_id", null: false
-    t.date "payment_date_to"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["creator_id"], name: "index_meetings_meeting_groups_on_creator_id"
-    t.index ["meeting_group_proposal_id"], name: "index_meetings_meeting_groups_on_meeting_group_proposal_id"
-  end
-
-  create_table "meetings_members", force: :cascade do |t|
-    t.string "login", null: false
-    t.string "email", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "name", null: false
-    t.uuid "identifier", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["identifier"], name: "index_meetings_members_on_identifier", unique: true
-  end
-
-  create_table "meetings_outboxes", force: :cascade do |t|
-    t.uuid "identifier", null: false
-    t.string "event", null: false
-    t.jsonb "payload"
-    t.string "aggregate", null: false
-    t.uuid "aggregate_identifier", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["aggregate_identifier"], name: "index_meetings_outboxes_on_aggregate_identifier"
-    t.index ["identifier"], name: "index_meetings_outboxes_on_identifier", unique: true
-  end
-
   create_table "transactional_outbox_outboxes", force: :cascade do |t|
     t.string "aggregate", null: false
     t.string "aggregate_identifier", null: false
@@ -174,7 +112,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_185646) do
     t.index ["reset_password_token"], name: "index_user_access_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "meetings_meeting_group_proposals", "meetings_members", column: "proposal_user_id"
-  add_foreign_key "meetings_meeting_groups", "meetings_meeting_group_proposals", column: "meeting_group_proposal_id"
-  add_foreign_key "meetings_meeting_groups", "meetings_members", column: "creator_id"
 end
